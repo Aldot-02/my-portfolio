0846b19f99a4409b2a2ce0bd949758e81bedb3b1
Aldo Twizerimana <aldotwizerimana@Aldos-MacBook-Pro.local>
2024-04-05 16:32:32 +0200

    removig blocking hindrance

--- Server/Routes/ProjectsRoute.ts
+++ Server/Routes/ProjectsRoute.ts
@@ -1,6 +1,5 @@
 import express, { Router } from 'express';
 import { createProject, deleteProject, getAllProjects, getProject, updateProject } from '../Controllers/ProjectsController';
-import { isAuthenticated } from '../Middlewares/middlewares';
 
 const router: Router = express.Router();
 
@@ -8,7 +7,7 @@ const router: Router = express.Router();
 router.post('/', createProject);
 router.get('/all', getAllProjects);
 router.get('/:id', getProject);
-router.patch('/:id', isAuthenticated, updateProject);
-router.delete('/:id', isAuthenticated, deleteProject);
+router.patch('/:id', updateProject);
+router.delete('/:id', deleteProject);
 
 export default router;
